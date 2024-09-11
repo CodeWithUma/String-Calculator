db541a64b3ec847a2231ab53bcf4a3b00a617ca7
Uma Prasad Das <umaprasaddas1999@gmail.com>
2024-09-11 21:42:43 +0530

    sum of multiple numbers

--- calculator.test.js
+++ calculator.test.js
@@ -15,4 +15,9 @@ describe('add function', () => {
         expect(add("1,2")).toBe(3);
         expect(add("3,7")).toBe(10);
     });
+
+    test('returns the sum of multiple numbers separated by commas', () => {
+        expect(add("1,2,3")).toBe(6);
+        expect(add("4,5,6,7")).toBe(22);
+    });
 });
